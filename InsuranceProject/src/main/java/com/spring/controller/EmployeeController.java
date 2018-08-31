035e370c3f4eee9dfab2eee778174e13a7172c24
chestermane <chestermane@gmail.com>
2018-08-31 15:58:13 -0400

    Evan's Commit

---
+++ InsuranceProject/src/main/java/com/spring/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+
+public class EmployeeController {
+
+	@RequestMapping
+	public String empLogin("/employeeLogin") {
+		return "employeeLogin";
+	}
+
+	@RequestMapping
+	public String viewClaims("/viewClaims") {
+		return "viewClaims";
+	}
+
+	@RequestMapping
+	public String updateClaim("/updateClaim") {
+		return "updateClaim";
+	}
+
+	@RequestMapping
+	public String viewReports("/viewReports") {
+		return "viewReports";
+	}
+
+	@RequestMapping
+		public String updateReport("/updateReport") {
+			return "updateReport";
+	}
+	
+}
