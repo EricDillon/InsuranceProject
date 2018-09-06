6b9d00d88f81b9cc3325dcaa2b5fef31df644c59
chestermane <chestermane@gmail.com>
2018-09-06 14:37:02 -0400

    Commit by Evan

--- InsuranceProject/src/main/java/com/spring/beanConfig/BeanConfig.java
+++ InsuranceProject/src/main/java/com/spring/beanConfig/BeanConfig.java
@@ -1,10 +1,11 @@
 package com.spring.beanConfig;
+
 import java.util.Properties;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
 
 public class BeanConfig {
 
@@ -20,22 +21,24 @@ public class BeanConfig {
 	public LocalSessionFactoryBean getSessionFactory() {
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource());
-		sessionFactory.setPackagesToScan("INSERT PACKAGE NAME HERE");
+		sessionFactory.setPackagesToScan(new String[] { "com.spring.model" });
 		sessionFactory.setHibernateProperties(hibernateProperties());
 		return sessionFactory;
 	}
 
-	private DataSource dataSource() {
+	@Bean
+	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassLoader("oracle.jdbc.driver.OracleDriver");
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
 		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
 		dataSource.setUsername("hr");
 		dataSource.setPassword("hr");
 		return dataSource;
 	}
 
-	 Properties hibernateProperties() {
+	Properties hibernateProperties() {
 		return new Properties() {
+			{
 			setProperty("hibernate.hbm2ddl.auto", "update");
 			setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
 			setProperty("hibernate.show_sql", "true");
