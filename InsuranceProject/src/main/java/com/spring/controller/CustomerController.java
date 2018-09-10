607357cb66e907fbad44f5a3ae3c6f4a11299e8d
chestermane <chestermane@gmail.com>
2018-09-10 15:04:45 -0400

    Check out regSuccess

--- InsuranceProject/src/main/java/com/spring/controller/CustomerController.java
+++ InsuranceProject/src/main/java/com/spring/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.spring.service.*;
@@ -16,50 +17,69 @@ public class CustomerController {
 	ClaimService cs;
 
 	@RequestMapping("/")
-	public String index(Model model) 
-	{
+	public String index(Model model) {
 		Customer c = new Customer();
 		model.addAttribute(c);
 		return "index";
 	}
-	
-	@RequestMapping("/clientLogin")
-	public String clientLogin() {
-		return "clientLogin";
+
+	@RequestMapping("/RegSuccess")
+	public String register(@ModelAttribute("customer") Customer customer, ModelMap model) {
+		model.addAttribute("firstName", customer.getFirstName());
+		model.addAttribute("email", customer.getEmail());
+		model.addAttribute("password", customer.getPassword());
+		
+		return "index";
+
 	}
+
+//	@RequestMapping("/clientLogin")
+//	public String clientLogin(@ModelAttribute("customer") Customer customer, Model model) {
+//		model.
+//		return "clientLogin";
+//	}
 	
+	  @RequestMapping("/clientLogin")
+	  public String clientLogin(@ModelAttribute("customer") Model model, 
+			  Customer customer) {
+		  model.addAttribute(customer.getUsername());
+		  model.addAttribute(customer.getPassword());
+		return "overview";
+	   
+	       
+	    
+	}
+
 	@RequestMapping("/overview")
 	public String overview() {
 		return "overview";
 	}
 
 	@RequestMapping("/makeclaim")
-	public String makeClaim(Model mod) 
-	{
-		
+	public String makeClaim(Model mod) {
+
 		mod.addAttribute(new Claim());
-		
+
 		return "customer/makeClaim";
 	}
-	
+
 	@RequestMapping("/checkClaim")
 	public String checkClaim() {
 		return "checkClaim";
 	}
-	
+
 	@RequestMapping("/myClaims")
 	public String myClaim() {
 		return "myClaims";
 	}
 
 	@RequestMapping("/submitClaim")
-	public String submitClaim(@ModelAttribute("claim") Claim cl, Model mod) 
-	{
+	public String submitClaim(@ModelAttribute("claim") Claim cl, Model mod) {
 		cs.addClaim(cl);
-		
+
 		Customer c = new Customer();
 		mod.addAttribute(c);
-		
+
 		return "index";
 	}
 
