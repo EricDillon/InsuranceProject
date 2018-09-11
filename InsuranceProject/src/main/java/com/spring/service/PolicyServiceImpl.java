904c17697911dbb57734515f21b0ee8c4f76b06e
David <david.grier@pyramidci.ccom>
2018-09-11 09:11:45 -0400

    k

--- InsuranceProject/src/main/java/com/spring/service/PolicyServiceImpl.java
+++ InsuranceProject/src/main/java/com/spring/service/PolicyServiceImpl.java
@@ -1,10 +1,16 @@
 package com.spring.service;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.spring.dao.PolicyDao;
 import com.spring.model.Policy;
+
+@Service
 public class PolicyServiceImpl implements PolicyService {
 
+	@Autowired
 	public int addPolicy(Policy p) {
 		// TODO Auto-generated method stub
 		return 0;
--- InsuranceProject/src/main/java/com/spring/service/ReportServiceImpl.java
+++ InsuranceProject/src/main/java/com/spring/service/ReportServiceImpl.java
@@ -2,10 +2,17 @@ package com.spring.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.spring.dao.ReportDao;
 import com.spring.model.Report;
+
+@Service
 public class ReportServiceImpl implements ReportService{
 
+	@Autowired
+	
 	public int addReport(Report r) {
 		// TODO Auto-generated method stub
 		return 0;
