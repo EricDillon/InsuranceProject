72bc99b34dd4f0a4883260de1e73371c999d45c6
chestermane <chestermane@gmail.com>
2018-09-11 15:28:24 -0400

    Please don't mess up

--- InsuranceProject/src/main/java/com/spring/controller/CustomerController.java
+++ InsuranceProject/src/main/java/com/spring/controller/CustomerController.java
@@ -1,10 +1,16 @@
 package com.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.spring.service.*;
 
 import com.spring.model.*;
@@ -13,53 +19,78 @@ import com.spring.model.*;
 public class CustomerController {
 
 	@Autowired
-	ClaimService cs;
-
+	ClaimService claimService;
+	
+	@Autowired
+	CustomerService customerService;
+	
 	@RequestMapping("/")
-	public String index(Model model) 
-	{
+	public String index(Model model) {
 		Customer c = new Customer();
 		model.addAttribute(c);
 		return "index";
 	}
+
+	@RequestMapping("/RegSuccess")
+	public String register(@ModelAttribute("customer") Customer customer, ModelMap model) {
+		model.addAttribute("firstName", customer.getFirstName());
+		model.addAttribute("email", customer.getEmail());
+		model.addAttribute("password", customer.getPassword());
+		
+		return "index";
+	}
 	
-//	@RequestMapping("/clientLogin")
-//	public String clientLogin() {
-//		return "clientLogin";
-//	}
-	
+	  @RequestMapping("/clientLogin")
+	  public String clientLogin(@ModelAttribute("customer") Model model, 
+			  Customer customer) {
+		  model.addAttribute(customer.getUsername());
+		  model.addAttribute(customer.getPassword());
+		  overview(customer.getUsername(), customer.getPassword());
+		return "overview";
+	   
+	       
+	    
+	}
+
 	@RequestMapping("/overview")
-	public String overview() {
+
+	public String overview(String username, String password) {
+		customerService.getCustomer(username, password);
 		return "customer/overview";
 	}
 
-	@RequestMapping("/makeclaim")
-	public String makeClaim(Model mod) 
-	{
-		
-		mod.addAttribute(new Claim());
-		
+
+	@RequestMapping(value="/makeclaim", method=RequestMethod.POST)
+	public String makeClaim(@ModelAttribute("claim") Model model) {
+		claimService.addClaim(new Claim());
 		return "customer/makeClaim";
 	}
-	
+
 	@RequestMapping("/checkClaim")
-	public String checkClaim() {
+	public String checkClaim(@ModelAttribute("claim") Model model) {
+		
+		//THIS NEEDS TO BE CHANGED
+		int id = 0;
+		claimService.getClaim(id);
+		//THIS NEEDS TO BE CHANGED
+		
 		return "checkClaim";
 	}
-	
+
 	@RequestMapping("/myClaims")
-	public String myClaim() {
+	public String myClaim(Model model) {
+		List<Claim> claims = claimService.viewClaims();
+		model.addAttribute("claims", claims);
 		return "myClaims";
 	}
 
 	@RequestMapping("/submitClaim")
-	public String submitClaim(@ModelAttribute("claim") Claim cl, Model mod) 
-	{
-		cs.addClaim(cl);
-		
+	public String submitClaim(@ModelAttribute("claim") Claim cl, Model mod) {
+		claimService.addClaim(cl);
+
 		Customer c = new Customer();
 		mod.addAttribute(c);
-		
+
 		return "index";
 	}
 
--- InsuranceProject/src/main/java/com/spring/controller/EmployeeController.java
+++ InsuranceProject/src/main/java/com/spring/controller/EmployeeController.java
@@ -1,13 +1,36 @@
 package com.spring.controller;
+import com.spring.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.service.EmployeeService;
+@Controller
 public class EmployeeController {
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+	@Autowired
+	Employee e;
 
 	@RequestMapping("/employeeLogin")
-	public String empLogin() {
-		return "employeeLogin";
+	public String empLogin(@ModelAttribute("employee") Model model, 
+			Employee employee) {
+		model.addAttribute(employee.getUsername());
+		  model.addAttribute(employee.getPassword());
+		  
+		return "employee/employeeLogin";
 	}
-
+	
+	@RequestMapping("/employeeOverview")
+	public String employeeOverview() {
+		return "employee/employeeOverview";
+		
+	}
+	
 	@RequestMapping("/viewClaims")
 	public String viewClaims() {
 		return "viewClaims";
